b0acbc683e832427770402c50850d841044e638f
akylbek.razakhov@gmail.com <akylbek.razakhov@gmail.com>
2022-09-15 11:25:05 +0600

    #StoryTask1-2

---
+++ Persistence/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
