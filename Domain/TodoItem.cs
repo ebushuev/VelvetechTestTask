092273e447dd26e733acc204ba556899f361a183
ibrag <ibragimov-amir@list.ru>
2022-09-24 00:54:59 +0600

    Add Libraries

---
+++ Domain/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain
+{
+  #region snippet
+  public class TodoItem
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string Secret { get; set; }
+  }
+  #endregion
+}
