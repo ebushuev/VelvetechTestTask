e67ffdd6824bac1537cea8abca284a6202b303c2
skoltsov <koltsov1998@yandex.ru>
2022-12-10 14:05:41 +0400

    added swagger

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using TodoApi.Database.DI;
-using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -31,6 +22,8 @@ namespace TodoApi
             services.AddDatabase(Configuration);
             
             services.AddControllers();
+            
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +31,8 @@ namespace TodoApi
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
