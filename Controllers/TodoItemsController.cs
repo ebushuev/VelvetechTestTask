7ec958095645764efb20a5d327f5ff19a389fb4d
alireza asgary <asgary_alireza@hotmail.com>
2022-12-19 21:57:47 +0300

    Initial merg : Task #1 * Add Datalayer & BusinessLayer to project * Add swagger * Add Third party log (serilog) *Add connection string to appsetting And migration database * Refactore Controller

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,9 +1,15 @@
+using BusinessLayer.DTOs;
+using BusinessLayer.Services.Interface;
+using DataLayer.Context;
+using DataLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TodoApi.Models;
+
 
 namespace TodoApi.Controllers
 {
@@ -11,56 +17,94 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
-        private readonly TodoContext _context;
+        private readonly ILogger<TodoItemsController> _logger;
+        private readonly ITodoItemService _todoItemService;
 
-        public TodoItemsController(TodoContext context)
+        public TodoItemsController(ITodoItemService toodoItemService, ILogger<TodoItemsController> logger)
         {
-            _context = context;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _todoItemService = toodoItemService;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
         {
-            return await _context.TodoItems
-                .Select(x => ItemToDTO(x))
-                .ToListAsync();
+            var todoItems = await _todoItemService.GetTodoItems();
+            try
+            {  
+                if (todoItems == null)
+                {
+                    _logger.LogInformation($"Empty Table");
+                    return NotFound();
+                }
+                else
+                {
+                    return new List<TodoItemDTO>(todoItems);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exeption error ", ex);
+                return StatusCode(500, "Server has problem");
+            }
+
+           ;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}",Name = "GetTodoItem")]
         public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
         {
-            var todoItem = await _context.TodoItems.FindAsync(id);
 
-            if (todoItem == null)
-            {
-                return NotFound();
-            }
+            var todoItem = await _todoItemService.GetTodoItem(id);
 
-            return ItemToDTO(todoItem);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateTodoItem(long id, TodoItemDTO todoItemDTO)
-        {
-            if (id != todoItemDTO.Id)
-            {
-                return BadRequest();
-            }
-
-            var todoItem = await _context.TodoItems.FindAsync(id);
-            if (todoItem == null)
-            {
-                return NotFound();
-            }
-
-            todoItem.Name = todoItemDTO.Name;
-            todoItem.IsComplete = todoItemDTO.IsComplete;
 
             try
             {
-                await _context.SaveChangesAsync();
+                if (todoItem == null)
+                {
+                    _logger.LogInformation($"Can not find ItemId={id}");
+                    return NotFound();
+                }
+                else
+                {
+                    return todoItem;
+                }
             }
-            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exeption error ", ex);
+                return StatusCode(500, "Server has problem");
+            }
+
+
+            
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodoItem(long id, CreateOrUpdateTodoItemDTO createOrUpdateTodoItemDTO)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                //todo add loging
+
+                return BadRequest();
+            }
+
+            var todoItem = await _todoItemService.GetTodoItem(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.Name = createOrUpdateTodoItemDTO.Name;
+            todoItem.IsComplete = createOrUpdateTodoItemDTO.IsComplete;
+
+            try
+            {
+                 _todoItemService.UpdateTodoItem(id, createOrUpdateTodoItemDTO);
+            }
+            catch (DbUpdateConcurrencyException) when (!_todoItemService.TodoItemExists(id))
             {
                 return NotFound();
             }
@@ -68,49 +112,63 @@ namespace TodoApi.Controllers
             return NoContent();
         }
 
+        
+
         [HttpPost]
-        public async Task<ActionResult<TodoItemDTO>> CreateTodoItem(TodoItemDTO todoItemDTO)
+        public async Task<ActionResult<TodoItemDTO>> CreateTodoItem(CreateOrUpdateTodoItemDTO todoItemDTO)
         {
-            var todoItem = new TodoItem
+            if (!ModelState.IsValid)
+            {
+                //todo add loging
+                return BadRequest();
+            }
+
+            var todoItem = new CreateOrUpdateTodoItemDTO()
             {
                 IsComplete = todoItemDTO.IsComplete,
                 Name = todoItemDTO.Name
             };
 
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
+         var id=   _todoItemService.CreateTodoItem(todoItem);
+       
 
             return CreatedAtAction(
                 nameof(GetTodoItem),
-                new { id = todoItem.Id },
-                ItemToDTO(todoItem));
+                new { id = id.Id },
+                ItemToDTO(todoItem,id.Id));
         }
+
+
+
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTodoItem(long id)
         {
-            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (!ModelState.IsValid)
+            {
+                //todo add loging
+                return BadRequest();
+            }
+
+            var todoItem = await _todoItemService.GetTodoItem(id);
 
             if (todoItem == null)
             {
                 return NotFound();
             }
-
-            _context.TodoItems.Remove(todoItem);
-            await _context.SaveChangesAsync();
-
+            _todoItemService.DeleteTodoItem(id);
             return NoContent();
         }
 
-        private bool TodoItemExists(long id) =>
-             _context.TodoItems.Any(e => e.Id == id);
 
-        private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+
+        private static TodoItemDTO ItemToDTO(CreateOrUpdateTodoItemDTO todoItem,long id) =>
             new TodoItemDTO
             {
-                Id = todoItem.Id,
+                Id = id,
                 Name = todoItem.Name,
                 IsComplete = todoItem.IsComplete
-            };       
+            };
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,21 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Services;
+using BusinessLayer.Services.Interface;
+using DataLayer.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using TodoApi.Models;
+using Serilog;
+
 
 namespace TodoApi
 {
+    
     public class Startup
     {
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,20 +34,62 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+
+
+            #region DataBase Context
+
+            services.AddDbContext<TodoContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ProjectConnectionString"));
+            });
+
+
+            #endregion
+
+            #region IoC
+
+            services.AddTransient<ITodoItemService, TodoItemService>();
+
+            #endregion
+        
+         
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "PAPServices API",
+                    Description = "PAP services for check black list personnel"
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/TodoProject.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "";
+            });
 
             app.UseRouting();
 
