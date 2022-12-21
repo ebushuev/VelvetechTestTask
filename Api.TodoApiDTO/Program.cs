0334570762751488f065571fa319aa9543198708
alireza asgary <asgary_alireza@hotmail.com>
2022-12-21 12:58:05 +0300

    Initial commit : Add DataLayer and BusinessLayer Add swagge Refactor project Add SeriLog (Third party)

--- Api.TodoApiDTO/Program.cs
+++ Api.TodoApiDTO/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace TodoApi
 {
@@ -21,6 +22,7 @@ namespace TodoApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .UseSerilog();
     }
 }
---
+++ DataLayer/Context/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Models;
+
+namespace DataLayer.Context
+{
+    public class TodoContext : DbContext
+    {
+
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<TodoItem> TodoItems { get; set; } 
+
+
+    }
+}
