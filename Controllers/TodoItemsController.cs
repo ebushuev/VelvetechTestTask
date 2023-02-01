d269778ce5ec5958664890fe8ab90c9c37c4b23c
Nodar Aronia <nodararonia@gmail.com>
2023-02-01 21:38:55 +0400

    NA: Created class library projects for project structure. Added Swagger, CORS policy and got rid of in memory database to make use of hosted SQL Server reading connection string from appsettings.json.

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,11 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TodoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApiDTO.Models;
 
-namespace TodoApi.Controllers
+namespace TodoApiDTO.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace TodoApi.Models
+namespace TodoApiDTO.Models
 {
     public class TodoContext : DbContext
     {
