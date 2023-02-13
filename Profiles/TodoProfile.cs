63243d88a53f995019970a389128e74fc2c5b36f
Ismayil Huseynli <Ismayil.Huseynli@mt.com>
2023-02-13 23:34:40 +0400

    adding profiler

---
+++ Profiles/TodoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TodoApi.Models;
+using TodoApiDTO.Dtos;
+
+
+namespace TodoApiDTO.Profiles
+{
+    public class TodoProfile : Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<TodoItem, TodoItemDTO>().ReverseMap();
+        }
+    }
+}
