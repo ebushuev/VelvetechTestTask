7370a46f80b7011ee3945b156637c37cce87530b
Sergey Tihomirov <ragej16@gmail.com>
2023-02-15 22:04:24 +0400

    Changed DI, added logging through the ExceptionFilter

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -1,14 +1,17 @@
 using Business;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoApiDTO.Infrastructure;
 using TodoApiDTO.Models;
 
 namespace TodoApiDTO.Controllers
 {
+    [ExceptionLoggingFilter]
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -33,7 +33,7 @@ namespace TodoApiDTO
             services.AddControllers();
             services.AddSwaggerGen();
 
-            services.AddScoped<ITodoService, TodoService>();
+            services.AddScoped<TodoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
