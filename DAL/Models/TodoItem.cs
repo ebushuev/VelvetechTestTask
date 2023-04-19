928b01b59129d4683555aa44046eaa0c783712c7
KabylbekovYers <kabylbekov.yers@gmail.com>
2023-04-19 16:38:01 +0600

    Task 1

---
+++ DAL/Models/TodoItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TodoApiDTO.DAL.Models
+{
+    public class TodoItem
+    {
+        public long Id {
+            get; set;
+        }
+        public string Name {
+            get; set;
+        }
+        public bool IsComplete {
+            get; set;
+        }
+        public string Secret {
+            get; set;
+        }
+
+        [Timestamp]
+        public byte[] Timestamp {
+            get; set;
+        }
+    }
+}
