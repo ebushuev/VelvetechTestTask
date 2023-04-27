77bc0cea718d262769d7949013e277b3a2045b9d
Evgenii Egorov <evgenii.egorov@outlook.com>
2023-04-27 20:47:36 +0300

    - Changed storage from 'InMemoryDB' to 'MS SQL Server'; - Connection string is stored in 'appsettings.json' file.

--- Startup.cs
+++ Startup.cs
@@ -23,8 +23,10 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<TodoContext>(opt =>
+            //   opt.UseInMemoryDatabase("TodoList"));
             services.AddDbContext<TodoContext>(opt =>
-               opt.UseInMemoryDatabase("TodoList"));
+                 opt.UseSqlServer(Configuration.GetConnectionString("Database")));
 
             services.AddDataRepositories()
                     .AddApplication();
