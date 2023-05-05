2353e02597b6ab890e129003408898d7e2800866
LeroKanz <108800662+LeroKanz@users.noreply.github.com>
2023-05-05 23:38:50 +0300

    Add files via upload

---
+++ TodoData/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoEntities.DbSet;
+
+namespace TodoData.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
