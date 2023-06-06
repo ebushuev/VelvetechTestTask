0f645b4925321b020b9644a2280d74bb81153b77
NursultanBekenov <nurbekenov.nb@gmail.com>
2023-06-07 01:07:05 +0500

    [Task-1]: added swagger, logger. DAL and BLL насколько смог. DB.

--- DTO/TodoItemDTO.cs
+++ DTO/TodoItemDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace TodoApi.Models
+﻿namespace TodoApiDTO.DTO
 {
     #region snippet
     public class TodoItemDTO
---
+++ Repo/ITodoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoApiDTO.Models;
+
+namespace TodoApiDTO.Repositories
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<TodoItem>> GetTodoItemsAsync();
+        Task<TodoItem> GetTodoItemAsync(long id);
+        void CreateTodoItem(TodoItem todoItem);
+        void DeleteTodoItem(TodoItem todoItem);
+        Task SaveChangesAsync();
+        bool TodoItemExists(long id);
+    }
+}
