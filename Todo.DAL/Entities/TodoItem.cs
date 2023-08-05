d479f1de9ccb885585288b2ff6d474c8ea651828
mablakulova <abmakhliyo@gmail.com>
2023-08-05 05:16:06 +0500

    added entity and dbcontext classes

---
+++ Todo.DAL/Entities/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Todo.DAL.Entities;
+
+public class TodoItem
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    public string? Secret { get; set; }
+}
