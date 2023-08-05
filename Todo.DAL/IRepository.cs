3b34fe9c2c3a3b3f86b7572e906ce480938580b7
mablakulova <abmakhliyo@gmail.com>
2023-08-05 05:19:03 +0500

    added repository classes

---
+++ Todo.DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Todo.DAL;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAll(bool trackChanges);
+
+    Task<T> GetByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+
+    Task SaveChanges();
+
+    void Create(T entity);
+
+    void Update(T entity);
+
+    void Delete(T entity);
+}
---
+++ Todo.DAL/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Todo.DAL.DbContexts;
+
+namespace Todo.DAL;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly TodoDbContext _dbContext;
+
+    public Repository(TodoDbContext dbContext)
+        => _dbContext = dbContext;
+
+    public async Task<IEnumerable<T>> GetAll(bool trackChanges)
+    {
+        IQueryable<T> query = !trackChanges ? _dbContext.Set<T>().AsNoTracking() : _dbContext.Set<T>();
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<T> GetByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+    {
+        IQueryable<T> query = !trackChanges ? _dbContext.Set<T>().Where(expression).AsNoTracking()
+            : _dbContext.Set<T>().Where(expression);
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public void Create(T entity) => _dbContext.Set<T>().Add(entity);
+
+    public void Update(T entity) => _dbContext.Set<T>().Update(entity);
+
+    public void Delete(T entity) => _dbContext.Set<T>().Remove(entity);
+
+    public async Task SaveChanges() => await _dbContext.SaveChangesAsync();
+}
