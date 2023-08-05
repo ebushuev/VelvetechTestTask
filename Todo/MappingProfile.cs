6d8a87b466e824c9b6ed24d5e487777301c17190
mablakulova <abmakhliyo@gmail.com>
2023-08-05 05:52:31 +0500

    added swagger, mapped dtos and configured extensions

---
+++ Todo/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Todo.Dtos;
+using Todo.DAL.Entities;
+
+namespace Todo;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<TodoItem, TodoItemDto>();
+
+        CreateMap<TodoItemForCreationDto, TodoItem>();
+
+        CreateMap<TodoItemForUpdateDto, TodoItem>().ReverseMap();
+    }
+}
