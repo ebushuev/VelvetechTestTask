b06e5114f45a9dc5e1c85e3c51cfd79dc4b9a285
Mikhail Prokopenko <prokopenko@cmsplanet.ru>
2023-08-18 18:26:47 +0400

    SQL Server used NuGet packages have been updated

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 using TodoApi.Models;
 
 namespace TodoApi
@@ -21,8 +22,10 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-               opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("DbConnection") ??
+                    throw new InvalidOperationException("Connection string 'DbConnection' not found.")));
+
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
