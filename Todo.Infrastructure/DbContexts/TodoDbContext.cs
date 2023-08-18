57ab28c999b073668421816d8bd40feaae63ee86
kogo <saba.koguashvili1999@mail.ru>
2023-08-18 11:55:43 +0400

    add entities and entityFramework

---
+++ Todo.Infrastructure/DbContexts/TodoDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Infrastructure.Entities;
+
+namespace Todo.Infrastructure.DbContexts;
+
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; }
+}
---
+++ Todo.Infrastructure/Entities/TodoItem.cs
@@ -0,0 +1,12 @@
+namespace Todo.Infrastructure.Entities;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    public string Secret { get; set; }
+}
