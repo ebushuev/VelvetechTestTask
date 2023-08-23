f8c6cb1fc8f8004716218b85c357e99a4a96fc14
Vasjen <vasjenm@gmail.com>
2023-08-23 15:07:05 +0400

    Added Swagger documentation to the project

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
 using TodoApi.Models;
 
 namespace TodoApi
@@ -30,6 +33,32 @@ namespace TodoApi
             services.AddDbContext<TodoContext>(opt =>
                opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Swagger TodoApi Documentation",
+                        Version = "v1",
+                        Description = "API Documentation for the TodoApi application",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Vasilii Mukhin",
+                            Email = "vasjenm@gmail.com"
+                        },
+                        Extensions = new Dictionary<string, IOpenApiExtension>
+                        {
+                          {"x-logo", new OpenApiObject
+                            {
+                               {"url", new OpenApiString("~/logo.png")},
+                               { "altText", new OpenApiString("TodoApi logo")}
+                            }
+                          }
+                        }
+
+                    });
+                options.EnableAnnotations();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +67,10 @@ namespace TodoApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options => 
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Swagger TodoApi Documentation v1"));
             }
 
             app.UseHttpsRedirection();
@@ -45,7 +78,7 @@ namespace TodoApi
             app.UseRouting();
 
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
